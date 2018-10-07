d6569bc4ac288d93f47bea0194b528d8d34f5c92
Maksimilian <maxas81@gmail.com>
2018-10-07 18:37:03 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import people from '../people';
+const $ = require('jquery');
+
+ $('body').append('<h1>'+people[1].name+'</h1>');
+
+console.log(people[0]);
---
+++ people.js
@@ -0,0 +1,10 @@
+// function getHello() {
+//     return 'Hello there';
+// }
+
+let people = [
+    {name:'John Doe'},
+    {name: 'Steve smith'}
+];
+
+module.exports = people;
